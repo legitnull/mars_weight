17c86ee5ac5f02def45a853cbadc8d72fb689c3b
legitnull <legitnull0@gmail.com>
2021-05-03 20:10:27 -0600

    commit on master

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    println!("Enter your weight (kg): ");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calc_weight(weight);
+    println!("Weight on Mars: {}kg", mars_weight);
+}
+
+fn calc_weight(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
+}
